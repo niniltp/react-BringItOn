cad4affac4b33203fb1d549a047e79ae9f3d445f
niniltp <nhienphan923@gmail.com>
2019-11-28 14:27:47 +0100

    compte field

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ const mediaTest = {
     description: "Teen Miles Morales becomes Spider-Man of his reality, crossing his path with five counterparts from other dimensions to stop a threat for all realities.",
     image: "http://fr.web.img6.acsta.net/r_1920_1080/pictures/18/11/13/11/42/1696141.jpg",
     actors: ["Shameik Moore", "Jake Johnson", "Hailee Steinfeld"],
-    duration: "1h 57m"
+    date: "Dec 14 2018",
+    duration: 117,
+    categories: ['animation', 'superheroes'],
+    language: "English",
+    rating: 4
 };
 
 function App() {
--- src/app/components/Media/MediaCard.js
+++ src/app/components/Media/MediaCard.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './MediaCard.css';
-import {Card, CardImg, CardText, CardTitle} from "reactstrap";
+import {Card, CardBody, CardImg, CardText, CardTitle} from "reactstrap";
+import {convertMinutesToTime} from "../../utilities/timeConverter";
 
 class MediaCard extends Component {
     constructor(props) {
@@ -23,7 +24,7 @@ class MediaCard extends Component {
         return {
             title: mediaData.title ? mediaData.title : '',
             description: mediaData.description ? mediaData.description : '',
-            types: mediaData.types ? mediaData.types : [],
+            categories: mediaData.categories ? mediaData.categories : [],
             duration: mediaData.duration ? mediaData.duration : '',
             date: mediaData.date ? mediaData.date : '',
             image: mediaData.image ? mediaData.image : '',
@@ -31,32 +32,48 @@ class MediaCard extends Component {
             rating: mediaData.rating ? mediaData.rating : '',
             actors: mediaData.actors ? mediaData.actors : '', // array
             artists: mediaData.artists ? mediaData.artists : '', // array
-            channel: mediaData.channel ? mediaData.channel : '', // array
+            channel: mediaData.channel ? mediaData.channel : '',
             producers: mediaData.producers ? mediaData.producers : '', // array
             directors: mediaData.directors ? mediaData.directors : '' // array
         }
     };
 
     displayCard = () => {
+        const durationInTime = this.state.media.duration ? convertMinutesToTime(this.state.media.duration) : 0;
         return (
             <div className="MediaCard">
                 <Card body inverse style={{backgroundColor: '#333'}}>
                     {this.state.media.image ?
                         <CardImg top width="100%" className="MediaCard-img" src={this.state.media.image}
                                  alt={"illustration of " + this.state.media.title}/> : ''}
-                    <div className="MediaCard-content">
-                        {this.state.media.title ? <CardTitle>{this.state.media.title}</CardTitle> : ''}
-                        {this.state.media.description ? <CardText>{this.state.media.description}</CardText> : ''}
+                    <CardBody className="MediaCard-content">
+                        {this.state.media.title ?
+                            <CardTitle className="MediaCard-title">{this.state.media.title}</CardTitle> : ''}
+                        {this.state.media.description ?
+                            <CardText className="MediaCard-body">{this.state.media.description}</CardText> : ''}
+                        {this.state.media.rating ?
+                            <small className="text-muted">Rating: {this.state.media.rating}/5<br/></small> : ''}
                         {this.state.media.date ?
-                            <CardText><small className="text-muted">Released
-                                date: {this.state.media.date}</small> {this.state.media.duration ? <small
-                                className="text-muted">({this.state.media.duration})</small> : ''}</CardText> : ''}
+                            <small className="text-muted">Released
+                                date: {this.state.media.date}</small> : ''}
+                        {this.state.media.duration && this.state.media.date ? <small
+                            className="text-muted"> ({durationInTime.hours}h {durationInTime.minutes}min)<br/></small> : ''}
                         {this.state.media.duration && !this.state.media.date ?
-                            <small className="text-muted">Duration: {this.state.media.duration}</small> : ''}
-
-                        {this.state.media.actors ? <CardText><small
-                            className="text-muted">Cast: {this.state.media.actors.join(', ')}</small></CardText> : ''}
-                    </div>
+                            <small
+                                className="text-muted">Duration: {durationInTime.hours}h {durationInTime.minutes}min<br/></small> : ''}
+                        {this.state.media.actors ? <small
+                            className="text-muted">Cast: {this.state.media.actors.join(', ')}<br/></small> : ''}
+                        {this.state.media.producers ? <small
+                            className="text-muted">Cast: {this.state.media.producers.join(', ')}<br/></small> : ''}
+                        {this.state.media.directors ? <small
+                            className="text-muted">Cast: {this.state.media.directors.join(', ')}<br/></small> : ''}
+                        {this.state.media.artists ? <small
+                            className="text-muted">Cast: {this.state.media.artists.join(', ')}<br/></small> : ''}
+                        {this.state.media.categories ? <small
+                            className="text-muted">Categories: {this.state.media.categories.join(', ')}<br/></small> : ''}
+                        {this.state.media.language ? <small
+                            className="text-muted">Language: {this.state.media.language}<br/></small> : ''}
+                    </CardBody>
                 </Card>
             </div>
         )
