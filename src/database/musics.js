82c236c65e73df9a22551431d55e803f21cf2401
Laura Flaquer <laura.flaquer@outlook.fr>
2019-11-28 16:52:49 +0100

    Musics database

---
+++ src/database/musics.js
@@ -0,0 +1,101 @@
+export default [
+    {
+        title: "Shape Of You",
+        duration: 4,
+        types: ["pop"],
+        artists: ["Ed Sheeran"],
+        date: "06/01/2017",
+        image: "https://m.media-amazon.com/images/I/A15CPGYubvL._SS500_.jpg",
+        language: "english",
+        rating: 3.4,
+        tags: ["pop"]
+    },
+    {
+        title: "I Still Haven't Found What I'm Looking For",
+        duration: 5,
+        types: ["rock"],
+        artists: ["U2"],
+        date: "01/06/1987",
+        image: "https://m.media-amazon.com/images/M/MV5BMGFmZjRmZTYtNWNhMy00MDRmLTlmNGQtMzA5ZjYzY2FkNjQ4XkEyXkFqcGdeQXVyNTk1NTMyNzM@._V1_SY1000_CR0,0,990,1000_AL_.jpg",
+        language: "english",
+        rating: 3.6,
+        tags: ["rock"]
+    },
+    {
+        title: "Fear Of The Dark",
+        duration: 7,
+        types: ["metal"],
+        artists: ["Iron Maiden"],
+        date: "11/05/1992",
+        image: "https://www.nuclearblast.de/static/articles/188/188992.jpg/1000x1000.jpg",
+        language: "english",
+        rating: 3.5,
+        tags: ["metal","halloween"]
+    },
+    {
+        title: "Circles",
+        duration: 3,
+        types: ["pop"],
+        artists: ["Post Malone"],
+        date: "03/09/2019",
+        image: "",
+        language: "english",
+        rating: 3.9,
+        tags: ["pop"]
+    },
+    {
+        title: "Crank That",
+        duration: 4,
+        types: ["Hip-hop","Rap"],
+        artists: ["Soulja Boy"],
+        date: "10/07/2007",
+        image: "https://images.genius.com/82f518b36b0958a230bbbd80fa7aaada.1000x1000x1.jpg",
+        language: "english",
+        rating: 2.2,
+        tags: ["Hip-hop","Rap"]
+    },
+    {
+        title: "Highway To Hell",
+        duration: 3,
+        types: ["Hard Rock"],
+        artists: ["AC/DC"],
+        date: "01/01/1979",
+        image: "https://m.media-amazon.com/images/I/71K1+8Xc7vL._SS500_.jpg",
+        language: "english",
+        rating: 3.9,
+        tags: ["Hard Rock"]
+    },
+    {
+        title: "Rise",
+        duration: 3,
+        types: ["Dance","Electronique"],
+        artists: ["Jonas Blue","Jack & Jack"],
+        date: "15/06/2018",
+        image: "https://www.lacoccinelle.net/1203726-1.jpg?20180825",
+        language: "english",
+        rating: 3.9,
+        tags: ["Dance","Electronique"]
+    },
+    {
+        title: "All I Want For Christmas Is You",
+        duration: 4,
+        types: ["holidays"],
+        artists: ["Mariah Carey"],
+        date: "01/11/2010",
+        image: "https://i.ytimg.com/vi/fRyhqobl0sk/maxresdefault.jpg",
+        language: "english",
+        rating: 3.7,
+        tags: ["holidays","christmas"]
+    },
+    {
+        title: "",
+        duration: ,
+        types: [""],
+        artists: [""],
+        date: "",
+        image: "",
+        language: "english",
+        rating: ,
+        tags: [""]
+    },
+];
