327e1bea717b4b6e34741d8a6a33283fb2794ba7
Laura Flaquer <laura.flaquer@outlook.fr>
2019-11-28 12:08:56 +0100

    Add files

---
+++ src/database/movies.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        title: "Spider-man: Into the Spider-Verse",
+        description: "Teen Miles Morales becomes Spider-Man of his reality, crossing his path with five counterparts from other dimensions to stop a threat for all realities.",
+        image: "http://fr.web.img6.acsta.net/r_1920_1080/pictures/18/11/13/11/42/1696141.jpg",
+        actors: ["Shameik Moore", "Jake Johnson", "Hailee Steinfeld"],
+        duration: "1h 57m"
+    },
+    {
+        id: 2,
+        wallet_id: 2,
+        amount: 3000
+    }
+];
