98ed956f6b729afbbdc768b3bed9861a4fda2eeb
niniltp <nhienphan923@gmail.com>
2019-12-26 15:50:42 +0100

    clean code

--- src/survey/Mood.js
+++ src/survey/Mood.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Row, Col, Button, ButtonGroup } from 'reactstrap';
+import React, {Component} from 'react';
+import {Button} from 'reactstrap';
 import './Survey.css';
 import './Button.css'
 import angry from '../img/angry.png'
@@ -7,7 +7,6 @@ import happy from '../img/happy.png'
 import sad from '../img/sad.png'
 import fine from '../img/fine.png'
 import soso from '../img/so-so.png'
-import Time from "./Time"
 import {Link} from "react-router-dom";
 
 class Mood extends Component {
@@ -22,60 +21,66 @@ class Mood extends Component {
 
     }
 
-    setRSelected = (i) => {    
-        this.setState({ rSelected : i} , () => {
+    setRSelected = (i) => {
+        this.setState({rSelected: i}, () => {
             this.selectMood();
         });
     }
 
     selectMood = () => {
-        if(this.state.rSelected === 1){
-            this.setState({ mood : "angry"});
+        if (this.state.rSelected === 1) {
+            this.setState({mood: "angry"});
         }
-        if(this.state.rSelected === 2){
-            this.setState({ mood : "sad"});
+        if (this.state.rSelected === 2) {
+            this.setState({mood: "sad"});
         }
-        if(this.state.rSelected === 3){
-            this.setState({ mood : "so_so"});
+        if (this.state.rSelected === 3) {
+            this.setState({mood: "so_so"});
         }
-        if(this.state.rSelected === 4){
-            this.setState({ mood : "fine"});
+        if (this.state.rSelected === 4) {
+            this.setState({mood: "fine"});
         }
-        if(this.state.rSelected === 5){
-            this.setState({ mood : "happy"});
+        if (this.state.rSelected === 5) {
+            this.setState({mood: "happy"});
         }
     }
 
-    next = () =>{
-        if( this.state.mood === "..."){
+    next = () => {
+        if (this.state.mood === "...") {
             return (
-                <Button className="btn-color-continue btn btn-animate" onClick={this.alertMood}><span>Continue</span></Button>
+                <Button className="btn-color-continue btn btn-animate"
+                        onClick={this.alertMood}><span>Continue</span></Button>
             )
-        }
-        else{
-            return(
-                <Link to={{pathname:'/time', state: {medias:this.state.medias, mood: this.state.mood}}}><Button className="btn-color-continue btn btn-animate"><span>Continue</span></Button></Link>
+        } else {
+            return (
+                <Link to={{pathname: '/time', state: {medias: this.state.medias, mood: this.state.mood}}}><Button
+                    className="btn-color-continue btn btn-animate"><span>Continue</span></Button></Link>
             )
         }
     }
 
-    alertMood = () =>{
+    alertMood = () => {
         alert("Choose your mood pls");
     }
 
-    render(){
+    render() {
         console.log(this.state.medias);
-        return(
+        return (
             <div>
                 <header className="Survey-header">
                     <h1>How are you doing today ?</h1>
                 </header>
                 <div className="Center">
-                    <Button className="Button-mood" onClick={() => this.setRSelected(1)} active={this.state.rSelected === 1}><img src={angry} alt="angryMood"/></Button>
-                    <Button className="Button-mood" onClick={() => this.setRSelected(2)} active={this.state.rSelected === 2}><img src={sad} alt="sadMood"/></Button>
-                    <Button className="Button-mood" onClick={() => this.setRSelected(3)} active={this.state.rSelected === 3}><img src={soso} alt="sosoMood"/></Button>
-                    <Button className="Button-mood" onClick={() => this.setRSelected(4)} active={this.state.rSelected === 4}><img src={fine} alt="fineMood"/></Button>
-                    <Button className="Button-mood" onClick={() => this.setRSelected(5)} active={this.state.rSelected === 5}><img src={happy} alt="happyMood"/></Button>
+                    <Button className="Button-mood" onClick={() => this.setRSelected(1)}
+                            active={this.state.rSelected === 1}><img src={angry} alt="angryMood"/></Button>
+                    <Button className="Button-mood" onClick={() => this.setRSelected(2)}
+                            active={this.state.rSelected === 2}><img src={sad} alt="sadMood"/></Button>
+                    <Button className="Button-mood" onClick={() => this.setRSelected(3)}
+                            active={this.state.rSelected === 3}><img src={soso} alt="sosoMood"/></Button>
+                    <Button className="Button-mood" onClick={() => this.setRSelected(4)}
+                            active={this.state.rSelected === 4}><img src={fine} alt="fineMood"/></Button>
+                    <Button className="Button-mood" onClick={() => this.setRSelected(5)}
+                            active={this.state.rSelected === 5}><img src={happy} alt="happyMood"/></Button>
                 </div>
                 <h4 className="select-Mood Center">I am {this.state.mood}</h4>
 
@@ -83,8 +88,8 @@ class Mood extends Component {
                     {this.next()}
                 </div>
             </div>
-        ); 
-    }  
+        );
+    }
 }
 
 export default Mood;
