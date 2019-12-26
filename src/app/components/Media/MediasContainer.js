ffd0eb3e8c8b96f1c97d4e990f2c523d83be3ff6
niniltp <nhienphan923@gmail.com>
2019-12-26 19:43:38 +0100

    try again button when no result

--- src/app/components/Media/MediasContainer.js
+++ src/app/components/Media/MediasContainer.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
-import {Col, Row} from "reactstrap";
+import {Button, Col, Row} from "reactstrap";
 import './MediaContainer.css';
 import MediaModal from "./MediaModal";
 import {firstLetterUpperCase} from "../../utilities/textFormater";
+import {Link} from "react-router-dom";
 
 class MediaContainer extends Component {
+    displayNoResult = () => {
+        return (<Col sm="12">
+            <h3 className="MediaContainer-noContent text-center">Sorry,
+                no {this.props.title} corresponding to your mood was found :c</h3>
+            <Link to={{pathname: '/'}}><Button className="btn-color-continue btn btn-animate">Try again</Button></Link>
+        </Col>)
+    };
+
     displayMediaContainer = () => {
         return (
             <div className="MediaContainer">
@@ -13,8 +22,7 @@ class MediaContainer extends Component {
                     {this.props.medias && this.props.medias.length > 0 ?
                         this.props.medias.map((media, key) => {
                             return <Col sm="4" key={key}><MediaModal media={media}/></Col>
-                        }) : <Col sm="12"><h3 className="MediaContainer-noContent text-center">Sorry,
-                            no {this.props.title} corresponding to your mood was found :c</h3></Col>}
+                        }) : this.displayNoResult()}
                 </Row>
             </div>
         )
