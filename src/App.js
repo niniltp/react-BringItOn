0ad14100269078d423a3ad03526c6eba789ad9ed
niniltp <nhienphan923@gmail.com>
2019-12-26 16:39:53 +0100

    add welcome page

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Mood from "./survey/Mood";
 import MediaChoice from "./app/components/Media/MediaChoice";
 import {BrowserRouter, Route} from 'react-router-dom';
 import Time from "./survey/Time";
+import Welcome from "./app/Scenes/Welcome/Welcome";
 
 function App() {
     return (
         <BrowserRouter>
-            <Route exact path="/" component={MediaChoice}/>
+            <Route exact path="/" component={Welcome}/>
+            <Route exact path="/medias" component={MediaChoice}/>
             <Route exact path="/mood" component={Mood}/>
             <Route exact path="/time" component={Time}/>
             <Route exact path="/results" component={MediaFinalResults}/>
---
+++ src/app/Scenes/Welcome/Welcome.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {Button, Fade} from "reactstrap";
+import {Link} from "react-router-dom";
+import './Welcome.css';
+
+class Welcome extends Component {
+    render() {
+        return (
+            <div className={"Welcome"}>
+                <Fade timeout={1000} style={{transition: '2s'}}>
+                    <h1>Hi !</h1>
+                </Fade>
+                <Fade timeout={2500} style={{transition: '2s'}}>
+                    <Link to={{pathname: '/medias'}}><Button
+                        className="btn-color-continue btn btn-animate"><span>Begin</span></Button></Link>
+                </Fade>
+            </div>
+        )
+    }
+}
+
+export default Welcome;
