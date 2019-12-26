e14eaf12238c6a1c9e0a2075914c096554ee6568
niniltp <nhienphan923@gmail.com>
2019-12-26 16:58:28 +0100

    fade in

--- src/app/components/Media/MediaChoice.js
+++ src/app/components/Media/MediaChoice.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './MediaChoice.css';
-import { Button, Container } from 'reactstrap';
+import {Button, Container, Fade} from 'reactstrap';
 import movie from '../../../img/clapperboard.svg';
 import music from '../../../img/vinyl.svg';
 import serie from '../../../img/home-theater.svg';
@@ -22,7 +22,7 @@ class MediaChoice extends Component {
     selectMedia = (media) => {
         const index = this.state.medias.indexOf(media);
 
-        if(index >= 0){
+        if (index >= 0) {
             const newMedia = this.state.medias.filter((m) => {
                 return m !== media;
             });
@@ -35,13 +35,11 @@ class MediaChoice extends Component {
 
             // this.state.media.splice(index, 1);
             console.log(this.state.medias);
-        }
-        else{
+        } else {
             const newMedia = this.state.medias.concat(media);
             this.setState({
                 medias: newMedia
             });
-            // this.state.media.push(media);
             console.log(this.state.medias);
         }
     };
@@ -50,48 +48,48 @@ class MediaChoice extends Component {
         return (
             <div className="Media">
                 <div className="row">
-                    <Button className="col-2" onClick={() => this.selectMedia("movies")} active={this.state.medias.includes("movies")}><img src={movie} alt="movies"/></Button>
-                    <Button className="col-2" onClick={() => this.selectMedia("series")} active={this.state.medias.includes("series")}><img src={serie} alt="series"/></Button>
-                    <Button className="col-2" onClick={() => this.selectMedia("musics")} active={this.state.medias.includes("musics")}><img src={music} alt="musics"/></Button>
+                    <Button className="col-2" onClick={() => this.selectMedia("movies")}
+                            active={this.state.medias.includes("movies")}><img src={movie} alt="movies"/></Button>
+                    <Button className="col-2" onClick={() => this.selectMedia("series")}
+                            active={this.state.medias.includes("series")}><img src={serie} alt="series"/></Button>
+                    <Button className="col-2" onClick={() => this.selectMedia("musics")}
+                            active={this.state.medias.includes("musics")}><img src={music} alt="musics"/></Button>
                 </div>
             </div>
         );
     };
 
-    buttonGoOn = () => {
-        return (
-            <div className="Validation">
-                <Button outline size="lg" color="secondary" >Bring it on !</Button>
-            </div>
-        )
-    };
-
-    next = () =>{
-        if( this.state.medias.length === 0){
+    next = () => {
+        if (this.state.medias.length === 0) {
             return (
                 <Button className="btn-color-continue btn btn-animate" onClick={this.alertMedias}><span>Continue</span></Button>
             )
-        }
-        else{
-            return(
-                <Link to={{pathname:'/mood', state: {medias:this.state.medias}}}><Button className="btn-color-continue btn btn-animate"><span>Continue</span></Button></Link>
+        } else {
+            return (
+                <Link to={{pathname: '/mood', state: {medias: this.state.medias}}}><Button
+                    className="btn-color-continue btn btn-animate"><span>Continue</span></Button></Link>
             )
         }
-    }
+    };
 
-    alertMedias = () =>{
+    alertMedias = () => {
         alert("Choose your medias pls");
-    }
+    };
 
     render() {
-        console.log(this.state.medias);
         return (
             <Container className="container-fluid">
-                <h1>What do you want to do ?</h1>
-                {this.buttonChoice()}
-                <div className="Validation">
-                    {this.next()}
-                </div>
+                <Fade timeout={1000} style={{transition: '2s'}}>
+                    <h1>What do you want to do ?</h1>
+                </Fade>
+                <Fade timeout={2000} style={{transition: '2s'}}>
+                    {this.buttonChoice()}
+                </Fade>
+                <Fade timeout={3000} style={{transition: '2s'}}>
+                    <div className="Validation">
+                        {this.next()}
+                    </div>
+                </Fade>
             </Container>
         )
     }
