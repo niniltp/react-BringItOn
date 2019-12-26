c9fe4028b788f2ca2428ecdfa8390bce5ea005e1
niniltp <nhienphan923@gmail.com>
2019-12-26 17:31:01 +0100

    text when no medias found

--- src/app/components/Media/MediasContainer.js
+++ src/app/components/Media/MediasContainer.js
@@ -9,9 +9,10 @@ class MediaContainer extends Component {
             <div className="MediaContainer">
                 <h2 className="MediaContainer-title">{this.props.title}</h2>
                 <Row>
-                    {this.props.medias.map((media, key) => {
-                        return <Col sm="4"  key={key}><MediaModal media={media}/></Col>
-                    })}
+                    {this.props.media && this.props.media.length > 0 ?
+                        this.props.medias.map((media, key) => {
+                            return <Col sm="4" key={key}><MediaModal media={media}/></Col>
+                        }) : <Col sm="12"> <h3 className="MediaContainer-noContent text-center">Sorry, no  {this.props.title[0].toLowerCase() + this.props.title.slice(1)} corresponding to your mood was found :c</h3></Col>}
                 </Row>
             </div>
         )
