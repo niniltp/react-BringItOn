7798d474bf34beb5329488f154aa0189abb7fda4
niniltp <nhienphan923@gmail.com>
2019-12-26 15:50:18 +0100

    link processing results to final display

--- src/survey/Time.js
+++ src/survey/Time.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Row, Col, Button, ButtonGroup, Container } from 'reactstrap';
+import React, {Component} from 'react';
+import {Button, Container} from 'reactstrap';
 import './Survey.css';
-import {Link} from "react-router-dom";
-import { determineResults } from "../app/service/processing"
+import {Redirect} from "react-router-dom";
+import {determineResults} from "../app/service/processing"
 
 class Time extends Component {
     constructor(props) {
@@ -13,77 +13,70 @@ class Time extends Component {
             mood: this.props.location.state.mood,
             rSelected: 0,
             time: "",
-            result: []
+            result: [],
+            redirect: false
         };
         console.log(this.state.medias);
         console.log(this.state.mood);
     }
 
     // 20, 45, 60, 120, 999 ==> string
-    setRSelected = (i) => {    
-        this.setState({ rSelected : i} , () => {
+    setRSelected = (i) => {
+        this.setState({rSelected: i}, () => {
             this.selectTime();
         });
-    }
+    };
 
     selectTime = () => {
-        if(this.state.rSelected === 1){
-            this.setState({ time : "20"});
+        if (this.state.rSelected === 1) {
+            this.setState({time: "20"});
+        } else if (this.state.rSelected === 2) {
+            this.setState({time: "45"});
+        } else if (this.state.rSelected === 3) {
+            this.setState({time: "60"});
+        } else if (this.state.rSelected === 4) {
+            this.setState({time: "120"});
+        } else if (this.state.rSelected === 5) {
+            this.setState({time: "999"});
         }
-        if(this.state.rSelected === 2){
-            this.setState({ time : "45"});
-        }
-        if(this.state.rSelected === 3){
-            this.setState({ time : "60"});
-        }
-        if(this.state.rSelected === 4){
-            this.setState({ time : "120"});
-        }
-        if(this.state.rSelected === 5){
-            this.setState({ time : "999"});
-        }
-    }
+    };
 
-    display = () =>{
-        if(this.state.rSelected === 1){
-            return("20 min !");
+    display = () => {
+        if (this.state.rSelected === 1) {
+            return ("20 min !");
         }
-        if(this.state.rSelected === 2){
-            return("45 min !");
+        if (this.state.rSelected === 2) {
+            return ("45 min !");
         }
-        if(this.state.rSelected === 3){
-            return("1h !");
+        if (this.state.rSelected === 3) {
+            return ("1h !");
         }
-        if(this.state.rSelected === 4){
-            return("2h !");
+        if (this.state.rSelected === 4) {
+            return ("2h !");
         }
-        if(this.state.rSelected === 5){
-            return("Whatever !");
-        }
-        else return(" ... ?")
-    }
+        if (this.state.rSelected === 5) {
+            return ("Whatever !");
+        } else return (" ... ?")
+    };
 
-    next = () =>{
-        if(this.state.time === ""){
-            return(
-                <Button className="btn btn-white btn-animate" onClick={() => this.alertTime()}>Surprise me !</Button>
-            )
-        }
-        else{
-            this.state.result = determineResults(this.state.mood,this.state.time,this.state.medias);
+    next = () => {
+        if (this.state.time === "") {
+            this.alertTime();
+        } else {
+            const results = determineResults(this.state.mood, this.state.time, this.state.medias);
 
-            return(
-                <Link to={{pathname:'/media_final_result', state: {medias:this.state.medias, mood: this.state.mood}}}> <Button className="btn btn-white btn-animate" onClick={() => this.next()}>Surprise me !</Button></Link>
-            )
+            this.setState({result: results}, () => {
+                this.setState({redirect: true})
+            });
         }
-    }
-  
-    alertTime = () =>{
+    };
+
+    alertTime = () => {
         alert("You didn't select a time :/");
-    }
+    };
 
-    render(){
-        return(
+    render() {
+        return (
             <Container>
                 <header className="Survey-header">
                     <h1>Do you have time ?</h1>
@@ -92,23 +85,28 @@ class Time extends Component {
                 <div className="Center">
                     <div className="Timeline"></div>
                 </div>
-                   
+
                 <div>
                     <ul className="a Center">
                         <li className="colorfont">
-                            <Button className="Button-time" onClick={() => this.setRSelected(1)} active={this.state.rSelected === 1}></Button>
+                            <Button className="Button-time" onClick={() => this.setRSelected(1)}
+                                    active={this.state.rSelected === 1}> </Button>
                         </li>
                         <li className="colorfont">
-                            <Button className="Button-time" onClick={() => this.setRSelected(2)} active={this.state.rSelected === 1}></Button>
+                            <Button className="Button-time" onClick={() => this.setRSelected(2)}
+                                    active={this.state.rSelected === 1}> </Button>
                         </li>
                         <li className="colorfont">
-                            <Button className="Button-time" onClick={() => this.setRSelected(3)} active={this.state.rSelected === 1}></Button>
+                            <Button className="Button-time" onClick={() => this.setRSelected(3)}
+                                    active={this.state.rSelected === 1}> </Button>
                         </li>
                         <li className="colorfont">
-                            <Button className="Button-time" onClick={() => this.setRSelected(4)} active={this.state.rSelected === 1}></Button>
+                            <Button className="Button-time" onClick={() => this.setRSelected(4)}
+                                    active={this.state.rSelected === 1}> </Button>
                         </li>
                         <li className="colorfont">
-                            <Button className="Button-time" onClick={() => this.setRSelected(5)} active={this.state.rSelected === 1}></Button>
+                            <Button className="Button-time" onClick={() => this.setRSelected(5)}
+                                    active={this.state.rSelected === 1}> </Button>
                         </li>
                     </ul>
                 </div>
@@ -128,12 +126,14 @@ class Time extends Component {
                 </div>
 
                 <div className="Continue-mood">
-                    {this.next()}
+                    <Button className="btn btn-white btn-animate" onClick={() => this.next()}>Surprise me !</Button>
+                    {this.state.redirect ? <Redirect
+                        to={{pathname: '/results', state: {medias: this.state.result, mood: this.state.mood}}}/> : ""}
                 </div>
 
-            </Container> 
-        ); 
-    }  
+            </Container>
+        );
+    }
 }
 
 export default Time;
