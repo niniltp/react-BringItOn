fd418ba30ca5b2a4febafa19f0c523c42db97266
niniltp <nhienphan923@gmail.com>
2020-01-09 10:20:32 +0100

    fix date begins at 0

--- src/app/utilities/datesPeriod.js
+++ src/app/utilities/datesPeriod.js
@@ -4,41 +4,41 @@ export function isPeriod(date, dateStart, dateEnd) {
 
 export function isChristmasPeriod(date) {
     // Christmas dates
-    const dateStart = new Date(date.getFullYear(), 11, 1);
-    const dateEnd = new Date(date.getFullYear(), 21, 26);
+    const dateStart = new Date(date.getFullYear(), 10, 1);
+    const dateEnd = new Date(date.getFullYear(), 11, 26);
 
     return isPeriod(date, dateStart, dateEnd);
 }
 
 export function isHalloweenPeriod(date) {
     // Halloween dates
-    const dateStart= new Date(date.getFullYear(), 9, 15);
-    const dateEnd = new Date(date.getFullYear(), 10, 15);
+    const dateStart= new Date(date.getFullYear(), 8, 15);
+    const dateEnd = new Date(date.getFullYear(), 9, 15);
 
     return isPeriod(date, dateStart, dateEnd);
 }
 
 export function isValentinePeriod(date) {
     // Valentine's day dates
-    const dateStart = new Date(date.getFullYear(), 9, 15);
-    const dateEnd = new Date(date.getFullYear(), 10, 15);
+    const dateStart = new Date(date.getFullYear(), 8, 15);
+    const dateEnd = new Date(date.getFullYear(), 9, 15);
 
     return isPeriod(date, dateStart, dateEnd);
 }
 
 export function isNewYearPeriod(date) {
     // New year's eve dates
-    const dateStart = new Date(date.getFullYear(), 1, 10);
-    const dateEnd = new Date(date.getFullYear(), 1, 17);
+    const dateStart = new Date(date.getFullYear(), 0, 10);
+    const dateEnd = new Date(date.getFullYear(), 0, 17);
 
     return isPeriod(date, dateStart, dateEnd);
 }
 
 export function isWinterPeriod(date) {
-    const dateStart = new Date(date.getFullYear(), 11, 15);
-    const dateEnd = new Date(date.getFullYear(), 12, 31);
-    const dateStart2 = new Date(date.getFullYear(), 1, 1);
-    const dateEnd2 = new Date(date.getFullYear(), 2, 20);
+    const dateStart = new Date(date.getFullYear(), 10, 15);
+    const dateEnd = new Date(date.getFullYear(), 11, 31);
+    const dateStart2 = new Date(date.getFullYear(), 0, 1);
+    const dateEnd2 = new Date(date.getFullYear(), 1, 20);
 
     return isPeriod(date, dateStart, dateEnd) || isPeriod(date, dateStart2, dateEnd2);
 }
